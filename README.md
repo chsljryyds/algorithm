e48d0b5c7e84eac9f8e8a5039c4de93e852a6125
llldd <2455567738@qq.com>
2025-03-31 23:01:46 +0800

    add new leetcode 题目

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # algorithm
+这是关于我的leetcode的题解说明   
+第二章到第五章是   算法（第四版） 书籍的代码案例
+leetcode文件是我刷题的题解
+其中部分题在leetcode题解笔记文件夹里
--- leetcode.py
+++ leetcode.py
@@ -1933,6 +1933,64 @@ class Solution:
 
 
 '''
+173. 二叉搜索树迭代器
+中等
+相关标签
+相关企业
+实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+int next()将指针向右移动，然后返回指针处的数字。
+注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+
+你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+
+ 
+
+示例：
 
 
+输入
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+输出
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+解释
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // 返回 3
+bSTIterator.next();    // 返回 7
+bSTIterator.hasNext(); // 返回 True
+bSTIterator.next();    // 返回 9
+bSTIterator.hasNext(); // 返回 True
+bSTIterator.next();    // 返回 15
+bSTIterator.hasNext(); // 返回 True
+bSTIterator.next();    // 返回 20
+bSTIterator.hasNext(); // 返回 False
 '''
+
+
+class BSTIterator():
+    def __init__(self, root: TreeNode):
+        # 初始化一个栈用于模拟中序遍历
+        self.stack = []
+        # 将所有最左侧节点入栈
+        self._push_left_nodes(root)
+
+    def _push_left_nodes(self, node):
+        # 辅助函数  用于将从当前节点到最左侧节点的路径上的所有节点入栈
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self):
+        # 弹出栈顶节点（当前要访问的节点）
+        node = self.stack.pop()
+        # 如果该节点有右子树，将右子树的所有最左侧节点入栈
+        self._push_left_nodes(node.right)
+        # 返回当前节点的值
+        return node.val
+
+    def hasNext(self):
+        # 如果栈不为空  说明还有节点未访问
+        return len(self.stack) > 0
